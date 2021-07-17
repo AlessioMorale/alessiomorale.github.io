8aa0c87c259aee952fb414bf79435fa9417c278e
Alessio Morale <alessiomorale@gmail.com>
2021-07-17 14:16:47 +0100

    fix: tweak styles and update site configuration for publishing

--- siteConfig.js
+++ siteConfig.js
@@ -1,8 +1,9 @@
 module.exports = {
   name: "A*Morale",
   shortName: "A*M",
-  description: "a*m thoughts trash can",
-  url: "https://alessiomorale.github.io", // Domain of your site without prefix!,
+  description:
+    "a*m thoughts trash can, a blog about robotics, drones, electronics and software engineering",
+  url: "https://blog.alessiomorale.com", // Domain of your site without prefix!,
   prefix: "/",
   author: "Alessio Morale",
   twitter: "alessiomorale",
